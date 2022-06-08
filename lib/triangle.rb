c08629c31f86ae26bef836c69140fbe3e3800721
princegobah <babynamek@gmail.com>
2022-06-07 23:17:04 -0400

    commit tree d2d6fe07ca003d3e4d240296b7f0e36c22fa3a5d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :arg1, :arg2, :arg3
+  def initialize(arg1, arg2, arg3)
+    @arg1=arg1
+    @arg2=arg2
+    @arg3=arg3
+  end
+
+  def kind
+    if @arg1<=0 ||@arg2<=0 ||@arg3<=0|| @arg1+@arg2<=@arg3|| @arg1+@arg3<=@arg2 ||@arg3+@arg2<=@arg1
+    
+      raise TriangleError
+    
+    end
+    if @arg1==@arg2 && @arg1==@arg3
+      :equilateral
+    elsif @arg1==@arg3|| @arg1==@arg2 ||@arg2==@arg3
+       :isosceles
+  
+    else 
+      :scalene
+    end
+   
+  end
+  
+  
+  
+
+  class TriangleError < StandardError
+
+  end
+
 end
+
+try=Triangle.new(0,1,2)
+
